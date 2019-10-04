c5f56fd2313711df98a1a5e5763d3fc7e026a0dd
Jakub Adam <jakub.adam@collabora.com>
2019-10-04 15:18:20 +0200

    recorder: add file created and completed signals

--- hwangsae/recorder.c
+++ hwangsae/recorder.c
@@ -28,11 +28,22 @@ struct _HwangsaeRecorder
 typedef struct
 {
   GstElement *pipeline;
+
+  gchar *recording_path;
 } HwangsaeRecorderPrivate;
 
 /* *INDENT-OFF* */
 G_DEFINE_TYPE_WITH_PRIVATE (HwangsaeRecorder, hwangsae_recorder, G_TYPE_OBJECT)
 /* *INDENT-ON* */
+
+enum
+{
+  FILE_CREATED_SIGNAL,
+  FILE_COMPLETED_SIGNAL,
+  LAST_SIGNAL
+};
+
+static guint signals[LAST_SIGNAL] = { 0 };
 
 HwangsaeRecorder *
 hwangsae_recorder_new (void)
@@ -50,13 +61,18 @@ hwangsae_recorder_start_recording (HwangsaeRecorder * self, const gchar * uri)
 
   g_return_if_fail (!priv->pipeline);
 
+  priv->recording_path =
+      g_strdup_printf ("/tmp/hwangsae-recording-%ld.mp4", g_get_real_time ());
+
   pipeline_str =
       g_strdup_printf
-      ("urisourcebin uri=%s ! tsdemux ! h264parse ! mp4mux ! filesink location=/tmp/hwangsae-recording-%ld.mp4",
-      uri, g_get_real_time ());
+      ("urisourcebin uri=%s name=srcbin ! tsdemux ! h264parse ! mp4mux ! filesink location=%s",
+      uri, priv->recording_path);
 
   priv->pipeline = gst_parse_launch (pipeline_str, &error);
   gst_element_set_state (priv->pipeline, GST_STATE_PLAYING);
+
+  g_signal_emit (self, signals[FILE_CREATED_SIGNAL], 0, priv->recording_path);
 }
 
 void
@@ -69,11 +85,21 @@ hwangsae_recorder_stop_recording (HwangsaeRecorder * self)
   // TODO: flush the pipeline contents, etc.
   gst_element_set_state (priv->pipeline, GST_STATE_NULL);
   gst_clear_object (&priv->pipeline);
+
+  g_signal_emit (self, signals[FILE_COMPLETED_SIGNAL], 0, priv->recording_path);
+  g_clear_pointer (&priv->recording_path, g_free);
 }
 
 static void
 hwangsae_recorder_class_init (HwangsaeRecorderClass * klass)
 {
+  signals[FILE_CREATED_SIGNAL] =
+      g_signal_new ("file-created", G_TYPE_FROM_CLASS (klass),
+      G_SIGNAL_RUN_LAST, 0, NULL, NULL, NULL, G_TYPE_NONE, 1, G_TYPE_STRING);
+
+  signals[FILE_COMPLETED_SIGNAL] =
+      g_signal_new ("file-completed", G_TYPE_FROM_CLASS (klass),
+      G_SIGNAL_RUN_LAST, 0, NULL, NULL, NULL, G_TYPE_NONE, 1, G_TYPE_STRING);
 }
 
 static void
