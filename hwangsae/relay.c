bde0121a756ddc7a263b0b9b213c0c283d90d631
Jakub Adam <jakub.adam@collabora.com>
2019-11-08 15:06:40 +0100

    relay: add "sink-port" and "source-port" properties

--- hwangsae/relay.c
+++ hwangsae/relay.c
@@ -18,23 +18,102 @@
 
 #include "relay.h"
 
+#include <gio/gio.h>
+
 struct _HwangsaeRelay
 {
   GObject parent;
+
+  GSettings *settings;
+
+  guint sink_port;
+  guint source_port;
 };
 
 /* *INDENT-OFF* */
 G_DEFINE_TYPE (HwangsaeRelay, hwangsae_relay, G_TYPE_OBJECT);
 /* *INDENT-ON* */
 
+enum
+{
+  PROP_SINK_PORT = 1,
+  PROP_SOURCE_PORT,
+  PROP_LAST
+};
+
+static void
+hwangsae_relay_finalize (GObject * object)
+{
+  HwangsaeRelay *self = HWANGSAE_RELAY (object);
+
+  g_clear_object (&self->settings);
+
+  G_OBJECT_CLASS (hwangsae_relay_parent_class)->finalize (object);
+}
+
+static void
+hwangsae_relay_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  HwangsaeRelay *self = HWANGSAE_RELAY (object);
+
+  switch (prop_id) {
+    case PROP_SINK_PORT:
+      self->sink_port = g_value_get_uint (value);
+      break;
+    case PROP_SOURCE_PORT:
+      self->source_port = g_value_get_uint (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+  }
+}
+
+static void
+hwangsae_relay_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  HwangsaeRelay *self = HWANGSAE_RELAY (object);
+  switch (prop_id) {
+    case PROP_SINK_PORT:
+      g_value_set_uint (value, self->sink_port);
+      break;
+    case PROP_SOURCE_PORT:
+      g_value_set_uint (value, self->source_port);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+  }
+}
+
 static void
 hwangsae_relay_class_init (HwangsaeRelayClass * klass)
 {
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+  gobject_class->set_property = hwangsae_relay_set_property;
+  gobject_class->get_property = hwangsae_relay_get_property;
+
+  g_object_class_install_property (gobject_class, PROP_SINK_PORT,
+      g_param_spec_uint ("sink-port", "SRT Binding port (from) ",
+          "SRT Binding port (from)", 0, G_MAXUINT, 8888,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
+  g_object_class_install_property (gobject_class, PROP_SOURCE_PORT,
+      g_param_spec_uint ("source-port", "SRT Binding port (to) ",
+          "SRT Binding port (to)", 0, G_MAXUINT, 9999,
+          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 }
 
 static void
 hwangsae_relay_init (HwangsaeRelay * self)
 {
+  self->settings = g_settings_new ("org.hwangsaeul.hwangsae.relay");
+
+  g_settings_bind (self->settings, "sink-port", self, "sink-port",
+      G_SETTINGS_BIND_DEFAULT);
+  g_settings_bind (self->settings, "source-port", self, "source-port",
+      G_SETTINGS_BIND_DEFAULT);
 }
 
 HwangsaeRelay *
