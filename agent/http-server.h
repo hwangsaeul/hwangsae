15e6dc982aeea7eaa3178ffd8b1bd0e87daaa84b
Walter Lozano <walter.lozano@collabora.com>
2019-11-20 09:23:36 -0300

    recording-agent: add http server

---
+++ agent/http-server.h
@@ -0,0 +1,38 @@
+/**
+ *  Copyright 2019 SK Telecom Co., Ltd.
+ *    Author: Jakub Adam <jakub.adam@collabora.com>
+ *    Author: Walter Lozano <walter.lozano@collabora.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#ifndef __HWANGSAE_HTTP_SERVER_H__
+#define __HWANGSAE_HTTP_SERVER_H__
+
+#include <glib-object.h>
+
+#define HWANGSAE_TYPE_HTTP_SERVER hwangsae_http_server_get_type()
+
+G_DECLARE_FINAL_TYPE (HwangsaeHttpServer, hwangsae_http_server, HWANGSAE, HTTP_SERVER, GObject)
+
+HwangsaeHttpServer *
+hwangsae_http_server_new ();
+
+gchar *
+hwangsae_http_server_get_recording_dir(HwangsaeHttpServer * server);
+
+void
+hwangsae_http_server_set_recording_dir(HwangsaeHttpServer * server, gchar *recording_dir);
+
+#endif /* __HWANGSAE_HTTP_SERVER_H__ */
