a21b7eb2d71b5b95473a1341ed8ea022670fa975
Jakub Adam <jakub.adam@collabora.com>
2020-08-20 18:38:51 +0200

    relay: increase send buffer of sink sockets
    Attempts to avoid "Non-blocking call failure: no buffer available for
    sending." on srt_send.

--- hwangsae/relay.c
+++ hwangsae/relay.c
@@ -132,6 +132,7 @@ static SrtParam srt_params[] = {
   {"SRTO_LINGER", SRTO_LINGER, 0},
   {"SRTO_TSBPMODE", SRTO_TSBPDMODE, 1}, /* Timestamp-based Packet Delivery */
   {"SRTO_RENDEZVOUS", SRTO_RENDEZVOUS, 0},      /* 0: not for rendezvous */
+  {"SRTO_SNDBUFLEN", SRTO_SNDBUF, 2 * 0xb80000},
   {NULL, -1, -1},
 };
 
