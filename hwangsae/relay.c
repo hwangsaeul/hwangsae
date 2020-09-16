ccf6804cc8f06f6aae69ebcad441f692d895fe9e
Jakub Adam <jakub.adam@collabora.com>
2020-09-16 14:52:21 +0200

    relay: close errorneous source connections

--- hwangsae/relay.c
+++ hwangsae/relay.c
@@ -795,6 +795,7 @@ _relay_main (gpointer data)
                   hwangsae_relay_emit_io_error_locked (self, source_socket,
                       HWANGSAE_RELAY_ERROR_WRITE, "srt_send failed: %s",
                       srt_strerror (srt_getlasterror (NULL), 0));
+                  _sink_connection_remove_source (sink, source_socket);
                 }
               }
             } else if (recv < 0) {
